4346d7fb6c7f6656c12e643cd652391bec310a65
Dmitri Dranishnikov <29133800+dmitridr@users.noreply.github.com>
2018-03-26 19:05:42 -0700

    Update FBProductFeed.php
    Remove extra newline.

--- app/code/community/Facebook/AdsExtension/Model/FBProductFeed.php
+++ app/code/community/Facebook/AdsExtension/Model/FBProductFeed.php
@@ -666,7 +666,6 @@ class FBProductFeed {
     if (!isset($this->current_currency)) {
       $this->current_currency = Mage::app()->getStore($this->store_id)->getCurrentCurrencyCode();
     }
-
     if ($this->base_currency === $this->current_currency) {
       return $price;
     }
