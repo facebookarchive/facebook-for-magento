5c5be627738e196f3309821050605a0d0b0cc112
Yu Li <liyuhk@fb.com>
2018-05-09 21:44:25 -0700

    fix the endpoint for msger chat in magento plugin after end to end testing
    Summary:
    as titled. Turns out I have missed to change the success tag, also in this diff added exception capture and logs.
    
    {F125320465}
    
    Reviewed By: mengyingdu
    
    Differential Revision: D7923314
    
    fbshipit-source-id: 2142345

--- app/code/community/Facebook/AdsExtension/controllers/Adminhtml/FbmsgerchatsetupController.php
+++ app/code/community/Facebook/AdsExtension/controllers/Adminhtml/FbmsgerchatsetupController.php
@@ -8,6 +8,12 @@
  * of patent rights can be found in the PATENTS file in the code directory.
  */
 
+if (file_exists(__DIR__.'/../../lib/fb.php')) {
+  include_once __DIR__.'/../../lib/fb.php';
+} else {
+  include_once __DIR__.'/../../../../Facebook_AdsExtension_lib_fb.php';
+}
+
 class Facebook_AdsExtension_Adminhtml_FbmsgerchatsetupController
   extends Mage_Adminhtml_Controller_Action {
 
@@ -22,34 +28,45 @@ class Facebook_AdsExtension_Adminhtml_FbmsgerchatsetupController
         'success' => false,
       );
 
-      $request = $this->getRequest();
+      try {
+        $request = $this->getRequest();
 
-      $is_messenger_chat_plugin_enabled = $request->getParam('is_messenger_chat_plugin_enabled');
-      if ($is_messenger_chat_plugin_enabled) {
-        Mage::getModel('core/config')->saveConfig(
-          'facebook_ads_toolbox/messengerchat/enabled',
-          $is_messenger_chat_plugin_enabled === 'true' ? '1' : '0');
+        $is_messenger_chat_plugin_enabled =
+          $request->getParam('is_messenger_chat_plugin_enabled');
+        if ($is_messenger_chat_plugin_enabled) {
+          Mage::getModel('core/config')->saveConfig(
+            'facebook_ads_toolbox/messengerchat/enabled',
+            $is_messenger_chat_plugin_enabled === 'true' ? '1' : '0');
+        }
+
+        $facebook_jssdk_version = $request->getParam('facebook_jssdk_version');
+        if ($facebook_jssdk_version) {
+          Mage::getModel('core/config')->saveConfig(
+            'facebook_ads_toolbox/fbjssdk/version',
+            $facebook_jssdk_version);
+        }
+
+        $page_id = $request->getParam('page_id');
+        if ($page_id) {
+          Mage::getModel('core/config')->saveConfig(
+            'facebook_ads_toolbox/messengerchat/pageid',
+            $page_id);
+        }
+
+        // TODO:(liyuhk) save other setups of msger chat if avaliable
+
+        $response['success'] = true;
+        FacebookAdsExtension::log("Messenger chat setup request received and saved!");
+      } catch (Exception $e) {
+        $response['success'] = false;
+        FacebookAdsExtension::logException($e);
       }
-
-      $facebook_jssdk_version = $request->getParam('facebook_jssdk_version');
-      if ($facebook_jssdk_version) {
-        Mage::getModel('core/config')->saveConfig(
-          'facebook_ads_toolbox/fbjssdk/version',
-          $facebook_jssdk_version);
-      }
-
-      $page_id = $request->getParam('page_id');
-      if ($page_id) {
-        Mage::getModel('core/config')->saveConfig(
-          'facebook_ads_toolbox/messengerchat/pageid',
-          $page_id);
-      }
-
-      // TODO:(liyuhk) save other setups of msger chat if avaliable
 
       $this->getResponse()->setHeader('Content-type', 'application/json');
-      $this->getResponse()->setBody(Mage::helper('core')->jsonEncode($response));
-    } else {
+      $this->getResponse()
+           ->setBody(Mage::helper('core')
+           ->jsonEncode($response));
+     } else {
       Mage::app()->getResponse()->setRedirect(
         Mage::helper('adminhtml')->getUrl(
           'adminhtml/fbmsgerchatsetup/index'));
