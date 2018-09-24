726a04265b3a259ae2988ff2d2b4962e29d39bbf
Nikolas Branis <nbran@kanenas.net>
2018-09-24 18:36:18 +0300

    Suggestion on fixing Issue #67
    Suggestion on fixing Issue #67.

--- app/code/community/Facebook/AdsExtension/Model/FBProductFeed.php
+++ app/code/community/Facebook/AdsExtension/Model/FBProductFeed.php
@@ -746,7 +746,12 @@ class FBProductFeed {
     if ($price === null) {
       $price = $product->getData('special_price');
     }
-    return $this->taxHelper->getPrice($product, $price);
+    $store = Mage::app()->getStore(1);
+    $taxCalculation = Mage::getModel('tax/calculation');
+    $request = $taxCalculation->getRateRequest(null, null, null, $store);
+    $taxClassId = $product->getTaxClassId();
+    $percent = $taxCalculation->getRate($request->setProductClassId($taxClassId));
+    return $this->taxHelper->getPrice($product, $price) + (($this->taxHelper->getPrice($product, $price) * $percent) / 100);
   }
 
   private function stripCurrencySymbol($price) {
