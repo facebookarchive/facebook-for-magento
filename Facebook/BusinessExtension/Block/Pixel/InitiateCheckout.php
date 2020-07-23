d066d691ea3b8c42f77d468079c246fe8380039b
José Antonio Jiménez <josejia@fb.com>
2020-07-23 15:37:21 -0700

    Adding missing fields to initiate checkout pixel event
    Summary:
    The contents and num_items field are sent in Conversions API events when a user initiates checkout, but they were missing on pixel events: https://developers.facebook.com/docs/facebook-pixel/reference#standard-events.
    A pair of methods were added in the associated block, and are called in the corresponding template. The regular expression in the functional test is changed too.
    
    Reviewed By: vinothsa
    
    Differential Revision: D22638201
    
    fbshipit-source-id: e2172eb0

--- Facebook/BusinessExtension/Block/Pixel/InitiateCheckout.php
+++ Facebook/BusinessExtension/Block/Pixel/InitiateCheckout.php
@@ -30,6 +30,37 @@ class InitiateCheckout extends Common {
     }
   }
 
+  public function getContents() {
+    $cart = $this->_fbeHelper->getObject('\Magento\Checkout\Model\Cart');
+    if(!$cart || !$cart->getQuote()) {
+      return '';
+    }
+    $contents = array();
+    $items = $cart->getQuote()->getAllVisibleItems();
+    $product_model = $this->_fbeHelper->getObject('\Magento\Catalog\Model\Product');
+    $priceHelper = $this->_objectManager->get('Magento\Framework\Pricing\Helper\Data');
+    foreach ($items as $item) {
+      $product = $product_model->load($item->getProductId());
+      $price = $priceHelper->currency($product->getFinalPrice(), false, false);
+      $content = '{id:"'.$product->getId().'",quantity:'.intval($item->getQty()).',item_price:'.$price."}";
+      $contents[] = $content;
+    }
+    return implode(',', $contents );
+  }
+
+  public function getNumItems() {
+    $cart = $this->_fbeHelper->getObject('\Magento\Checkout\Model\Cart');
+    if(!$cart || !$cart->getQuote()) {
+      return null;
+    }
+    $numItems = 0;
+    $items = $cart->getQuote()->getAllVisibleItems();
+    foreach ($items as $item) {
+      $numItems += $item->getQty();
+    }
+    return $numItems;
+  }
+
   public function getEventToObserveName(){
     return 'facebook_businessextension_ssapi_initiate_checkout';
   }
